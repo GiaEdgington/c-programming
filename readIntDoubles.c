50e8febbb3cc4329272087fa91b874b4405f15e5
giaedgington <gia.edgington@gmail.com>
2021-05-02 21:53:34 -0400

    Capture int and doubles

---
+++ readIntDoubles.c
@@ -0,0 +1,14 @@
+//capturing integers and doubles
+
+
+#include <stdio.h>
+int main(void) {
+    int age;
+    double height;
+    printf("What is your age?");
+    scanf("%d",&age);
+    printf("What is your height?");
+    scanf("%lf",&height);
+    printf("You are %d years old and %.2lf meters tall.",age,height);
+    return 0;
+}
