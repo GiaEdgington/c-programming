1512c8fa08f02507e133579bd709f3e2c2acd9a2
giaedgington <gia.edgington@gmail.com>
2021-05-03 21:00:37 -0400

    Divide doubles and int

---
+++ divide.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void){
+    double celcius;
+    scanf("%lf", &celcius);
+    printf("%.1lf", celcius * 9.0 / 5.0 + 32.0);
+    return 0;
+}
+
+//Temperature (°F) = Temperature (°C) × 9.0 / 5.0 + 32.0
