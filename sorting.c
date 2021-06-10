7f815a3a7d7c8f2782de34221fb28e4bf11eaf17
giaedgington <gia.edgington@gmail.com>
2021-06-09 22:28:17 -0400

    Find length of string

--- sorting.c
+++ sorting.c
@@ -3,17 +3,30 @@
 int main(void){
     char word[50];
     scanf("%s", word);
-    int i, j;
+    int i=0;
+    int l=0;
+    while (word[i]!='\0') {
+        l++;
+        i++;
+    };
+    int j;
     char letter;
-    
-    for(j=0;j<strlen(word)-1;j++){
-        for(i=0; i<strlen(word)-1;i++){
-            if(word[i] > word[i+1]){
-                letter = word[i];
-                word[i] = word[i+1];
-                word[i]= letter;
-            }
+    int k=0;
+    int same = 0;
+   
+    for(j=0;j<(l-1);j++){
+        for(i=0; i<(l-1);i++){
+        if(word[i] > word[i+1]){
+            letter = word[i];
+            word[i] = word[i+1];
+            word[i]= letter;
+        } else if(word[i] == word[i+1]){
+            letter = word[i];
+            k+=1;
         }
+       }
     }
+    printf("%d", k);
+    
     return 0;
 }
